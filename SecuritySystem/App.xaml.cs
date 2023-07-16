48b78d30f7579ab22f46b307d63ab19c4ff25f9b
Anand Avinash Bhalerao <anandbhalerao.l.a3b@gmail.com>
2023-07-16 17:31:40 +0530

    Add project files.

---
+++ SecuritySystem/App.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
+using Firebase.Database;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MVVMEssentials.Services;
+using MVVMEssentials.Stores;
+using MVVMEssentials.ViewModels;
+using SecuritySystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace SecuritySystem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IHost _host;
+        public App()
+        {
+            _host = Host
+                .CreateDefaultBuilder()
+                .ConfigureServices((context, service) =>
+                {
+                    string firebaseKey = "AIzaSyAYnGaFSaBsTcTYMHn8RzRijTvuvL_3484";
+                    FirebaseAuthConfig config = new FirebaseAuthConfig
+                    {
+                        ApiKey = firebaseKey,
+                        AuthDomain = "security-system-a7530.firebaseapp.com",
+                        Providers = new FirebaseAuthProvider[]
+                            {
+                                new EmailProvider()
+                            }
+                    };
+
+                    service.AddSingleton<FirebaseAuthClient>(new FirebaseAuthClient(config));
+
+                    service.AddSingleton<NavigationStore>();
+                    service.AddSingleton<ModalNavigationStore>();
+
+                    service.AddSingleton<MainViewModel>();
+
+                    service.AddSingleton<NavigationService<RegisterAdminViewModel>>(
+                        (services) => new NavigationService<RegisterAdminViewModel>(
+                            services.GetRequiredService<NavigationStore>(),
+                            () => new RegisterAdminViewModel
+                                (
+                                    services.GetRequiredService<FirebaseAuthClient>(),
+                                    services.GetRequiredService<FirebaseClient>()
+                                )
+                            ));
+
+                    
+
+                    service.AddSingleton<MainWindow>((services) => new MainWindow()
+                    {
+                        //DataContext = new RegisterAdminViewModel(services.GetRequiredService<FirebaseAuthClient>())
+                        DataContext = services.GetRequiredService<MainViewModel>()
+
+                    });
+
+                    service.AddSingleton<FirebaseClient>(new FirebaseClient("https://security-system-a7530-default-rtdb.firebaseio.com"));
+
+
+                })
+
+                .Build();
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            INavigationService navigationServie = _host.Services.GetRequiredService<NavigationService<RegisterAdminViewModel>>();
+            navigationServie.Navigate();
+
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ SecuritySystem/ViewModels/RegisterAdminViewModel.cs
@@ -0,0 +1,111 @@
+﻿using Firebase.Auth;
+using Firebase.Database;
+using MVVMEssentials.ViewModels;
+using SecuritySystem.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SecuritySystem.ViewModels
+{
+    public class RegisterAdminViewModel : ViewModelBase
+    {
+        private string email;
+        public string Email
+        {
+            get 
+            {
+                return email; 
+            }
+            set 
+            {
+                email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        private string locationType;
+        public string LocationType
+        {
+            get
+            {
+                return locationType;
+            }
+            set
+            {
+                locationType = value;
+                OnPropertyChanged(nameof(LocationType));
+            }
+        }
+
+        private string location;
+        public string Location
+        {
+            get
+            {
+                return location;
+            }
+            set
+            {
+                location = value;
+                OnPropertyChanged(nameof(Location));
+            }
+        }
+
+        private string phoneNo;
+        public string PhoneNo
+        {
+            get
+            {
+                return phoneNo;
+            }
+            set
+            {
+                phoneNo = value;
+                OnPropertyChanged(nameof(PhoneNo));
+            }
+        }
+
+
+        public ICommand RegisterCommand { get; }
+
+        public ICommand NavigateLoginCommand { get; }
+
+        public RegisterAdminViewModel(FirebaseAuthClient client, FirebaseClient dataClient)
+        {
+            RegisterCommand = new RegisterAdminCommand(this, client,dataClient);
+        }
+
+    }
+}
